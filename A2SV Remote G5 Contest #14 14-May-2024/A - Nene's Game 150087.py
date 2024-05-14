8322db1eac29debbcd8a5725c22c03acc573f1cd
Meriem RAHOU <lm_rahou@esi.dz>
2024-05-14 18:31:48 +0100

    Created 'A - Nene's Game' by A2SV Hub

---
+++ A2SV Remote G5 Contest #14 14-May-2024/A - Nene's Game 150087.py
@@ -0,0 +1,22 @@
+# Problem: A - Nene's Game - https://codeforces.com/gym/523225/problem/A
+
+testcases = int(input())
+
+for _ in range(testcases):
+    k, q = map(int, input().split())
+
+    a = list(map(int, input().split()))
+    ns = list(map(int, input().split()))
+
+    res = []
+
+    for n in ns:
+        acop = a.copy()
+        while acop and n > 0:
+            if acop[-1] > n:
+                acop.pop()
+            else:
+                n -= len(acop)
+        res.append(n)
+    print(*res)
+
