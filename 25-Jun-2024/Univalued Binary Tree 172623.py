a27936177aa99dd84ca5620a8589d717cd488b02
Meriem RAHOU <lm_rahou@esi.dz>
2024-06-25 19:48:54 +0100

    Created 'Univalued Binary Tree' by A2SV Hub

---
+++ 25-Jun-2024/Univalued Binary Tree 172623.py
@@ -0,0 +1,32 @@
+# Problem: Univalued Binary Tree - https://leetcode.com/problems/univalued-binary-tree/description/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+
+        # Initialize the queue with the root node
+        queue = deque([root])
+        
+        val = root.val
+        
+        # Perform BFS
+        while queue:
+            node = queue.popleft()
+            
+            if node.val != val:
+                return False
+            
+            if node.left:
+                queue.append(node.left)
+            
+            if node.right:
+                queue.append(node.right)
+        
+        return True
