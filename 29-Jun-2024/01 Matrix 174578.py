236a484635147c29562c641ecb39bf264fee72a4
Meriem RAHOU <lm_rahou@esi.dz>
2024-06-29 19:09:53 +0100

    Created '01 Matrix' by A2SV Hub

---
+++ 29-Jun-2024/01 Matrix 174578.py
@@ -0,0 +1,29 @@
+# Problem: 01 Matrix - https://leetcode.com/problems/01-matrix/
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        if not mat or not mat[0]:
+            return mat
+
+        rows, cols = len(mat), len(mat[0])
+        dist = [[float('inf')] * cols for _ in range(rows)]
+        queue = deque()
+        
+        for r in range(rows):
+            for c in range(cols):
+                if mat[r][c] == 0:
+                    dist[r][c] = 0
+                    queue.append((r, c))
+        
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        
+        while queue:
+            r, c = queue.popleft()
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols:
+                    if dist[nr][nc] > dist[r][c] + 1:
+                        dist[nr][nc] = dist[r][c] + 1
+                        queue.append((nr, nc))
+        
+        return dist
