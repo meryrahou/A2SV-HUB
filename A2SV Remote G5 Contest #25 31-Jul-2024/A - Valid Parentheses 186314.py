dac2c6d8f9bb763ec81cf8486dab0ccfa36abc1c
Meriem ( Mery ) RAHOU <lm_rahou@esi.dz>
2024-07-31 19:30:48 +0100

    Created 'A - Valid Parentheses' by A2SV Hub

---
+++ A2SV Remote G5 Contest #25 31-Jul-2024/A - Valid Parentheses 186314.py
@@ -0,0 +1,16 @@
+# Problem: A - Valid Parentheses - https://codeforces.com/gym/537575/problem/A
+
+list = list(map(str, input()))
+
+stack = []
+count = 0
+for i in range(len(list)):
+    if list[i] == "(":
+        stack.append("(")
+    
+    elif list[i] == ")":
+        if stack and stack[-1] == "(":
+            stack.pop()
+            count += 1
+
+print(count*2)
