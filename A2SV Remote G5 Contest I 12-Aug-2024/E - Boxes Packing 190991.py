bc38f5e4f9d71cff27a7d43498c9634247f372e8
Meriem ( Mery ) RAHOU <lm_rahou@esi.dz>
2024-08-12 20:21:51 +0100

    Created 'E - Boxes Packing' by A2SV Hub

---
+++ A2SV Remote G5 Contest I 12-Aug-2024/E - Boxes Packing 190991.py
@@ -0,0 +1,19 @@
+# Problem: E - Boxes Packing - https://codeforces.com/gym/500425/problem/E
+
+n = int(input())
+k = list(map(int, input().split()))
+
+# second thought
+logdict = {}
+for i in range(n):
+    if k[i] in logdict:
+        logdict[k[i]] += 1
+    else:
+        logdict[k[i]] = 1
+print(max(logdict.values()))
+
+
+
+# initial submission : wrong on test case 4
+#kset = set(k)
+# print( n - len(kset) + 1)
