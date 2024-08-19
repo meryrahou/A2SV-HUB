e1b380a873e695c61207cc8dfd1a469efee1ac80
Meriem ( Mery ) RAHOU <lm_rahou@esi.dz>
2024-08-19 23:11:56 +0100

    Created 'Jump Game' by A2SV Hub

---
+++ 19-Aug-2024/Jump Game 198058.py
@@ -0,0 +1,11 @@
+# Problem: Jump Game - https://leetcode.com/problems/jump-game/
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        maxReach = 0
+        for i in range(len(nums)):
+            if i > maxReach:
+                return False
+            maxReach = max(maxReach, i + nums[i])
+        return True
