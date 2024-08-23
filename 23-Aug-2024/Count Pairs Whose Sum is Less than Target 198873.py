055c71fbe3b4d752e6a20ff6937b77c6991b7d4c
Meriem ( Mery ) RAHOU <lm_rahou@esi.dz>
2024-08-23 01:15:14 +0100

    Created 'Count Pairs Whose Sum is Less than Target' by A2SV Hub

---
+++ 23-Aug-2024/Count Pairs Whose Sum is Less than Target 198873.py
@@ -0,0 +1,17 @@
+# Problem: Count Pairs Whose Sum is Less than Target - https://leetcode.com/problems/count-pairs-whose-sum-is-less-than-target/
+
+class Solution:
+    def countPairs(self, nums: List[int], target: int) -> int:
+        nums.sort()  
+        left = 0
+        right = len(nums) - 1
+        count = 0
+        
+        while left < right:
+            if nums[left] + nums[right] < target:
+                count += (right - left)  
+                left += 1
+            else:
+                right -= 1
+        
+        return count
