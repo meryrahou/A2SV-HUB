4c5439af7ab2972ed5fbd66fef422609262b63d5
Meriem ( Mery ) RAHOU <lm_rahou@esi.dz>
2024-08-30 22:20:43 +0100

    Created 'Binary Number with Alternating Bits' by A2SV Hub

---
+++ 30-Aug-2024/Binary Number with Alternating Bits 209674.py
@@ -0,0 +1,11 @@
+# Problem: Binary Number with Alternating Bits - https://leetcode.com/problems/binary-number-with-alternating-bits/
+
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        bin_n = bin(n)[2:]
+        
+        for i in range(1, len(bin_n)):
+            if bin_n[i] == bin_n[i - 1]:
+                return False
+                
+        return True
