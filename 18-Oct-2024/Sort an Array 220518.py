e4e910e17f223c5ff70b187edbaf877003633c47
Meriem ( Mery ) RAHOU <lm_rahou@esi.dz>
2024-10-18 13:27:52 +0100

    Created 'Sort an Array' by A2SV Hub

---
+++ 18-Oct-2024/Sort an Array 220518.py
@@ -0,0 +1,5 @@
+# Problem: Sort an Array - https://leetcode.com/problems/sort-an-array/description/
+
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        return sorted(nums)
