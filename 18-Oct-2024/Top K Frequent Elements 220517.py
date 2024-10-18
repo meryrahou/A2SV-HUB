27a2e35e27e12894d701af2b9088e138c6610b77
Meriem ( Mery ) RAHOU <lm_rahou@esi.dz>
2024-10-18 13:26:45 +0100

    Created 'Top K Frequent Elements' by A2SV Hub

---
+++ 18-Oct-2024/Top K Frequent Elements 220517.py
@@ -0,0 +1,7 @@
+# Problem: Top K Frequent Elements - https://leetcode.com/problems/top-k-frequent-elements/
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = Counter(nums)
+    
+        return heapq.nlargest(k, count.keys(), key=count.get)
